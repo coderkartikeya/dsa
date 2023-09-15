c6ad024d565f6ebe266ae72e24be39a2ea55a769
kartikeya vats <pandatjibagpatvale@gmail.com>
2023-09-15 23:56:08 +0530

    learning about pointers

---
+++ 15SeptemberPracticeWhole.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+void print(int **p){
+    **p++;
+
+}
+
+int main(){
+//     int arr[2][2]={{1,2},{3,4}};
+//     int ar[2][2];
+    
+        
+        
+//    cout<<&arr<<endl;
+    // int x=10;
+    // int *ptr=&x;
+    // cout<<*ptr<<endl;
+    // x++;
+    // cout<<*ptr<<endl;
+    // cout<<x<<endl;
+    // cout<<"hello"<<endl;
+
+    // call by reference
+
+    int c=10;
+    int *ptr=&c;
+    cout<<ptr<<endl;
+    print(&ptr);
+    cout<<ptr<<endl;
+
+
+
+
+}
